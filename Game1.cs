43a8c1194ba85cbe6086330087f66901299a5911
dragonskaterdx666 <susiethingos@gmail.com>
2020-03-08 18:41:42 +0000

    struct

--- Game1.cs
+++ Game1.cs
@@ -6,43 +6,51 @@ namespace MsPacMan
 {
     public class Game1 : Game
     {
+        #region Variables
+
         GraphicsDeviceManager graphics;
+
         SpriteBatch spriteBatch;
-        
+
+        #endregion
+
+        #region Constuctor
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+
             Content.RootDirectory = "Content";
         }
 
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
+        /// This is where it can query for any required services and load any non-graphic related content.  
+        /// Calling base.Initialize will enumerate through any components and initialize them as well.
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        /// LoadContent will be called once per game and is the place to load all of your content.
         /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
+        /// UnloadContent will be called once per game and is the place to unload game-specific content.
         /// </summary>
         protected override void UnloadContent()
         {
@@ -50,16 +58,13 @@ namespace MsPacMan
         }
 
         /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
+        /// Allows the game to run logic such as updating the world,checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -72,9 +77,11 @@ namespace MsPacMan
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
+        #endregion
+
     }
+
 }

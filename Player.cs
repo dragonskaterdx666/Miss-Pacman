4d7663354ca1e781ff78c3489355918f8c98b7fb
mari128tz <oliveira.mariana128@gmail.com>
2020-03-13 17:47:14 +0000

    blayer

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MsPacMan
+{
+    public class Player /*: DrawableGameComponent*/
+    {
+
+        //Variables
+        enum Direction
+        {
+            Up, Down, Right, Left
+        }
+
+        Texture2D texture;
+        SpriteBatch spriteBatch;
+        Game1 game;
+
+        #region Methods
+        public Player(Game1 game, int x, int y) 
+        {
+            //DrawOrder = 100; //Player should be the last object to draw
+        }
+
+        //public override void Update()
+        //{
+
+        //}
+
+        #endregion
+    }
+}
